1d250da6893381d36e83a690d4d9bdb9f7415544
julien.fiaffe <julien.fiaffe@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-11-01 10:52:47 +0000

    Gestion de la connexion via le service OData : à améliorer.

--- RSSFeedWeb/Controllers/HomeController.cs
+++ RSSFeedWeb/Controllers/HomeController.cs
@@ -8,28 +8,19 @@ namespace RSSFeedWeb.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
-            // Code pour le cryptage en MD5, à mettre dans une classe outil
-            //
-            //MD5 md5 = new MD5CryptoServiceProvider();
-            //Byte[] originalBytes = Encoding.UTF8.GetBytes("mypass");
-            //Byte[] encodedBytes = md5.ComputeHash(originalBytes);
+            RSSFeedService.RSSFeedDatabaseEntities ctx = new RSSFeedService.RSSFeedDatabaseEntities(new Uri("http://localhost:3152/RSSFeedDataService.svc/"));
 
-            //StringBuilder result = new StringBuilder();
-
-            //for (int i = 0; i < encodedBytes.Length; i++)
-            //{
-            //    // Affiche le Hash en hexadecimal 
-            //    result.Append(encodedBytes[i].ToString("X2"));
-            //}
-            //ViewBag.Pass = result.ToString();
-
-            return View();
+            var categories = from m in ctx.CATEGORY
+                        select m;
+            return View(categories);
         }
 
+        
         public ActionResult About()
         {
             return View();
---
+++ RSSFeedWeb/Tools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSSFeedWeb
+{
+    public class Tools
+    {
+        public static string MD5Hash(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            Byte[] originalBytes = Encoding.UTF8.GetBytes(str);
+            Byte[] encodedBytes = md5.ComputeHash(originalBytes);
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < encodedBytes.Length; i++)
+            {
+                result.Append(encodedBytes[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+    }
+}
