e0c5bca1b3bc8db73baf21bd81c6954cb6cd1f79
julien.fiaffe <julien.fiaffe@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-11-03 15:26:38 +0000

    Mise à jour de la base SQL avec de nouvelles informations.

--- trunk/SQL/data_user.sql
+++ trunk/SQL/data_user.sql
@@ -1,11 +1,14 @@
 INSERT INTO [RSSFeedDatabase].[dbo].[USER]
            ([user_email]
            ,[user_password]
+           ,[user_connected]
            ,[status_id]
            ,[role_id])
      VALUES
            ('admin@admin.com'
            ,CONVERT(VARCHAR(32), HashBytes('MD5', 'admin'), 2)
+           ,0
+		   ,CONVERT(VARCHAR(32), HashBytes('MD5', 'admin@admin.com'), 2)
            ,1
            ,1)
 GO
