981348e59ec1d84035fc30fc515d51ffb821bb72
julien.fiaffe <julien.fiaffe@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-11-04 17:19:18 +0000

    Modification de la table USER pour prendre en compte le type de client utilisé.

--- trunk/SQL/data_status.sql
+++ trunk/SQL/data_status.sql
@@ -1,17 +1,17 @@
 INSERT INTO [RSSFeedDatabase].[dbo].[STATUS]
            ([status_name])
      VALUES
+           ('invalid')
+
+		   INSERT INTO [RSSFeedDatabase].[dbo].[STATUS]
+           ([status_name])
+     VALUES
            ('valid')
 
 INSERT INTO [RSSFeedDatabase].[dbo].[STATUS]
            ([status_name])
      VALUES
            ('closed')
-           
-INSERT INTO [RSSFeedDatabase].[dbo].[STATUS]
-           ([status_name])
-     VALUES
-           ('invalid')
 GO
 
 
