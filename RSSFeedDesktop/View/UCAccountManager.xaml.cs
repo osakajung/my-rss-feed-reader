3670ea3ce725a137c07bcc17457a8b6a81d20d0a
dauptain.c@gmail.com <dauptain.c@gmail.com@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-11-06 12:23:13 +0000

    regeneration du model edmx

---
+++ RSSFeedDesktop/View/UCAccountManager.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSSFeedDesktop
+{
+	/// <summary>
+	/// Logique d'interaction pour UCAccountManager.xaml
+	/// </summary>
+	public partial class UCAccountManager : UserControl
+	{
+		public UCAccountManager()
+		{
+			this.InitializeComponent();
+		}
+
+		private void TextBlock_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			// TODO : ajoutez ici l’implémentation du gestionnaire d’événements.
+		}
+	}
+}
---
+++ RSSFeedDesktop/View/UCRssBookManager.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSSFeedDesktop
+{
+	/// <summary>
+	/// Logique d'interaction pour UCRssBookManager.xaml
+	/// </summary>
+	public partial class UCRssBookManager : UserControl
+	{
+		public UCRssBookManager()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
