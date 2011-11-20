aa103f716d15cddb4fe1c41cdda0d111a5632524
dauptain.c@gmail.com <dauptain.c@gmail.com@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-11-20 18:21:03 +0000

    avancement du client WPF

--- RSSFeedDesktop/ViewModel/AccountVM.cs
+++ RSSFeedDesktop/ViewModel/AccountVM.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using RSSFeedModel;
 using System.Windows.Input;
+using System.Windows.Controls;
+using System.ComponentModel.DataAnnotations;
 
 namespace RSSFeedDesktop.ViewModel
 {
@@ -13,18 +15,27 @@ namespace RSSFeedDesktop.ViewModel
         private LogOnModel _logOn;
         private RegisterModel _register;
         public event EventHandler LoginCompleted;
+        public event EventHandler RegisterCompleted;
 
         private ICommand _logInCommand;
+        private ICommand _registerCommand;
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                if (_registerCommand == null)
+                    _registerCommand = new RelayCommand<object>(RegisterAction, RegisterCanExecute);
+                return _registerCommand;
+            }
+        }
 
         public ICommand LogInCommand
         {
             get
             {
                 if (_logInCommand == null)
-                {
-                    _logInCommand = new RelayCommand<object>(LoginAction, null);
-                }
-
+                    _logInCommand = new RelayCommand<object>(LoginAction, LoginCanExecute);
                 return _logInCommand;
             }
         }
@@ -61,12 +72,42 @@ namespace RSSFeedDesktop.ViewModel
             Register = new RegisterModel();
         }
 
+        private bool LoginCanExecute(object param)
+        {
+            List<System.ComponentModel.DataAnnotations.ValidationResult> validationResults = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            ValidationContext vc = new ValidationContext(LogOn, null, null);
+            if (Validator.TryValidateObject(LogOn, vc, validationResults, true) && this.LoginCompleted != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
         private void LoginAction(object param)
         {
-            if (/* LogOn.LogOn(RSSFeedModel.AccountService.ClientType.DesktopClient) && */ this.LoginCompleted != null)
+            if (true/* LogOn.LogOn(RSSFeedModel.AccountService.ClientType.DesktopClient) && */)
             {
                 email = LogOn.UserEmail;
                 this.LoginCompleted.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private bool RegisterCanExecute(object param)
+        {
+            List<System.ComponentModel.DataAnnotations.ValidationResult> validationResults = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            ValidationContext vc = new ValidationContext(Register, null, null);
+            if (Validator.TryValidateObject(Register, vc, validationResults, true) && this.RegisterCompleted != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void RegisterAction(object param)
+        {
+            if (true/* Register.Register() && */)
+            {
+                //this.RegisterCompleted.Invoke(this, EventArgs.Empty);
             }
         }
     }
