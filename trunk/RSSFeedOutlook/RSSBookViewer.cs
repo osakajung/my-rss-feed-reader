a68003126e2576038fe4afb59030e850208a5829
dauptain.c@gmail.com <dauptain.c@gmail.com@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-12-09 18:37:45 +0000

    client outlook done

---
+++ trunk/RSSFeedOutlook/RSSBookViewer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace RSSFeedOutlook
+{
+    partial class RSSBookViewer
+    {
+        #region Form Region Factory
+
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass("IPM.Post.RssBook")]
+        [Microsoft.Office.Tools.Outlook.FormRegionName("RSSFeedOutlook.RSSBookViewer")]
+        public partial class RSSBookViewerFactory
+        {
+            // Occurs before the form region is initialized.
+            // To prevent the form region from appearing, set e.Cancel to true.
+            // Use e.OutlookItem to get a reference to the current Outlook item.
+            private void RSSBookViewerFactory_FormRegionInitializing(object sender, Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs e)
+            {
+            }
+        }
+
+        #endregion
+
+        // Occurs before the form region is displayed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void RSSBookViewer_FormRegionShowing(object sender, System.EventArgs e)
+        {
+        }
+
+        // Occurs when the form region is closed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void RSSBookViewer_FormRegionClosed(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
