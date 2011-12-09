710f59d84b7768721ae7f6a1462c34525a8793a7
julien.fiaffe@gmail.com <julien.fiaffe@gmail.com@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-12-09 16:38:25 +0000

    Lecture d'un flux disponible sur le client WindowsPhone, version beta, il y a BonjourMadame !

---
+++ trunk/RSSFeedPhone/Models/FeedModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace RSSFeedPhone.Models
+{
+    public class FeedModel
+    {
+        public Int64 Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+
+        public string Description { get; set; }
+
+        public long NonReadItems { get; set; }
+
+        public FeedModel() { }
+
+        public FeedModel(DataService.FEED feed)
+        {
+            this.Id = feed.feed_id;
+            this.Address = feed.feed_address;
+            this.Title = feed.feed_title;
+            this.Link = feed.feed_link;
+            this.Description = feed.feed_description;
+        }
+    }
+}
