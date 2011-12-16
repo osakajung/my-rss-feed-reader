576e4367c6d22365325479d5e241a85cf50596ca
dauptain.c@gmail.com <dauptain.c@gmail.com@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-12-16 11:55:34 +0000

    version locale finale

--- RSSFeedDesktop/Model/RegisterModel.cs
+++ RSSFeedDesktop/Model/RegisterModel.cs
@@ -11,13 +11,13 @@ namespace RSSFeedDesktop.Model
         public string Email { get; set; }
 
         [Required]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
         
         [Required]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         public string ConfirmPassword { get; set; }
