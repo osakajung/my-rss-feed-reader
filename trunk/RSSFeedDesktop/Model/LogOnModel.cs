affc06b756beec214bbecad008fc5ead626f80fc
dauptain.c@gmail.com <dauptain.c@gmail.com@ac7756f5-7fbd-dfa0-063d-cde0d11e0fc2>
2011-12-16 11:55:34 +0000

    version locale finale

--- trunk/RSSFeedDesktop/Model/LogOnModel.cs
+++ trunk/RSSFeedDesktop/Model/LogOnModel.cs
@@ -11,7 +11,7 @@ namespace RSSFeedDesktop.Model
         public string UserEmail { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
